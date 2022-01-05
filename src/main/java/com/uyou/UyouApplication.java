b47e912b3623b704fdd294e6f66be2c7a2e348ce
wzx <569159845@qq.com>
2022-01-05 14:21:37 +0800

    Initial commit

---
+++ src/main/java/com/uyou/UyouApplication.java
@@ -0,0 +1,13 @@
+package com.uyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UyouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uyou/UyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

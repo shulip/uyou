fbe8fd3f93ce2abab7d53d38d0784ddd0fcbf3bc
zhangyj6 <1006419490@qq.com>
2022-03-23 21:07:20 +0800

    添加测试用例

--- src/test/java/com/uyou/service/impl/ProjectServiceImplTest.java
+++ src/test/java/com/uyou/service/impl/ProjectServiceImplTest.java
@@ -2,15 +2,25 @@ package com.uyou.service.impl;
 
 import com.uyou.dao.ProjectMapper;
 import com.uyou.dao.UserMapper;
+import com.uyou.dto.*;
+import com.uyou.entity.Project;
+import com.uyou.entity.ProjectProducer;
 import com.uyou.helperdao.GameTypeHelperMapper;
 import com.uyou.helperdao.ProducerTypeHelperMapper;
 import com.uyou.helperdao.ProjectHelperMapper;
 import com.uyou.helperdao.ProjectProducerHelperMapper;
 import com.uyou.service.ProjectService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootTest
 class ProjectServiceImplTest {
@@ -28,34 +38,89 @@ class ProjectServiceImplTest {
     private ProjectProducerHelperMapper projectProducerHelperMapper;
     @Resource
     private UserMapper userMapper;
-//    @Test
-//    void createProject() {
-//        ProducerTypeDTO p = new ProducerTypeDTO("设计",7);
-//        projectService.createProject(new ProjectCreateDTO("哈利波特","魔法觉醒",
-//                new ArrayList<String>(Arrays.asList("冒险", "卡牌")),
-//                new ArrayList<ProducerTypeDTO>(Collections.singletonList(p))));
-//    }
-//
-//    @Test
-//    void joinProject() {
-//    }
-//    @Test
-//    void getAllProject() {
-//        List<Project> projects = projectHelperMapper.selectAll();
-//        for(Project project:projects){
-//            System.out.println(project.getId());
-//        }
-//    }
-//
-//    @Test
-//    void getSelfProject() {
-//    }
-//
-//
-//    @Test
-//    void getDetailProject() {
-//        Integer projectId = 119150743;
-//        List<ProjectProducer> projectProducers = projectProducerHelperMapper.selectByProjectId(projectId);
-//        int a=1;
-//    }
+
+    @Test
+    @Transactional
+    @Rollback()
+    void createProject() {
+        ProducerTypeDTO p = new ProducerTypeDTO("设计",7);
+        if(
+                projectService.createProject(new ProjectCreateDTO("哈利波特","魔法觉醒",
+                    new ArrayList<>(Arrays.asList("冒险", "角色扮演")),
+                    new ArrayList<>(Collections.singletonList(p))))
+        )
+            System.out.println("哈利波特项目创建成功");
+        else System.out.println("哈利波特项目创建失败");
+
+        ProducerTypeDTO p1 = new ProducerTypeDTO("美术",5);
+        ProducerTypeDTO p2 = new ProducerTypeDTO("运营",2);
+        if(
+                projectService.createProject(new ProjectCreateDTO("吃鸡","",
+                    new ArrayList<>(Arrays.asList("动作", "冒险")),
+                    new ArrayList<>(Arrays.asList(p,p1,p2))))
+        )
+            System.out.println("吃鸡项目创建成功");
+        else System.out.println("吃鸡项目创建失败");
+    }
+
+    @Test
+    @Transactional
+    @Rollback()
+    void joinProject() {
+        if(
+                projectService.joinProject(128223328,"策划_")
+        )
+            System.out.println("加入项目成功");
+        else System.out.println("加入项目失败");
+
+        if(
+                projectService.joinProject(130163006,"程序员")
+        )
+            System.out.println("加入项目成功");
+        else System.out.println("加入项目失败");
+    }
+
+    @Test
+    @Transactional
+    @Rollback()
+    void getAllProject() {
+        List<ProjectDTO> projects = projectService.getAllProject();
+        for(ProjectDTO project:projects){
+            System.out.println(project.toString());
+        }
+    }
+
+    @Test
+    @Transactional
+    @Rollback()
+    void getSelfProject() {
+        ProjectSelfDTO projectSelfDTO= projectService.getSelfProject();
+        for(ProjectDTO project:projectSelfDTO.getCreate()){
+            System.out.println(project.toString());
+        }
+        for(ProjectDTO project:projectSelfDTO.getJoin()){
+            System.out.println(project.toString());
+        }
+    }
+
+
+    @Test
+    @Transactional
+    @Rollback()
+    void getDetailProject() {
+        ProjectDetailDTO projectDetailDTO = projectService.getDetailProject(119150743);
+        System.out.println(projectDetailDTO.toString());
+
+        projectDetailDTO = projectService.getDetailProject(128223328);
+        System.out.println(projectDetailDTO.toString());
+
+        projectDetailDTO = projectService.getDetailProject(130163006);
+        System.out.println(projectDetailDTO.toString());
+
+        projectDetailDTO = projectService.getDetailProject(130164511);
+        System.out.println(projectDetailDTO.toString());
+
+        projectDetailDTO = projectService.getDetailProject(225208331);
+        System.out.println(projectDetailDTO.toString());
+    }
 }
--- src/test/java/com/uyou/service/impl/UserServiceImplTest.java
+++ src/test/java/com/uyou/service/impl/UserServiceImplTest.java
@@ -1,12 +1,18 @@
 package com.uyou.service.impl;
 
 import com.uyou.dao.UserMapper;
+import com.uyou.dto.UserDTO3;
 import com.uyou.dto.UserNameTypeDTO;
 import com.uyou.entity.User;
+import com.uyou.service.ProjectService;
+import com.uyou.service.UserService;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -16,24 +22,58 @@ class UserServiceImplTest {
     private UserMapper userMapper;
     @Autowired
     private StringEncryptor stringEncryptor;
+    @Autowired
+    private UserService userService;
 
-//    @Test
-//    void getUserById() {
-//        User user = userMapper.selectByPrimaryKey(5);
-//        UserNameTypeDTO userNameTypeDTO = new UserNameTypeDTO(user.getName(), user.getType());
-//        int a=1;
-//    }
-//
-//    @Test
-//    void modifyUser() {
-//        System.out.print(stringEncryptor.encrypt("aaa"));
-////        User user = new User();
-////        user.setId(5);
-////        user.setName("x");
-////        user.setPassword(stringEncryptor.encrypt("hhh"));
-////        user.setType("程序员");
-////        userMapper.updateByPrimaryKey(user);
-////        user = userMapper.selectByPrimaryKey(5);
-//        int a=1;
-//    }
+    @Test
+    @Transactional
+    @Rollback()
+    void getUserById() {
+        UserNameTypeDTO userNameTypeDTO = userService.getUserById();
+        System.out.println(userNameTypeDTO.toString());
+    }
+
+    @Test
+    @Transactional
+    @Rollback()
+    void modifyUser() {
+        UserDTO3 userDTO3 = new UserDTO3("b", "bbbbbb", "运营");
+        userService.modifyUser(userDTO3);
+    }
+
+    @Test
+    @Transactional
+    @Rollback()
+    void register() {
+        try {
+            System.out.println(userService.register("x", "xxxx", "美术"));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println(userService.register("zyj", "zyjzyj", "程序员"));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @Transactional
+    @Rollback()
+    void loadUserByUsername() {
+        try {
+            UserDetails userDetails = userService.loadUserByUsername("x");
+            System.out.println(userDetails.toString());
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            UserDetails userDetails = userService.loadUserByUsername("zyxwvu");
+            System.out.println(userDetails.toString());
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
